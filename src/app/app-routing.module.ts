70d0a56c96eb57c2f2ebe0c6b1ff0342ab2f0f17
Sebastian Gamba <sebastiangamba.music@gmail.com>
2021-11-29 21:26:44 -0500

    plantilla sitio web basico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './plantilla/error/error.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
+import { MisionComponent } from './plantilla/mision/mision.component';
+import { VisionComponent } from './plantilla/vision/vision.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"inicio",
+    component: InicioComponent
+  },
+  {
+    path: "mision",
+    component: MisionComponent
+  },
+  {
+    path: "vision",
+    component: VisionComponent
+  },
+  {
+    path:"",
+    pathMatch: 'full',
+    redirectTo: '/inicio'
+  },
+  {
+    path: '**',
+    component: ErrorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './plantilla/header/header.component';
+import { FooterComponent } from './plantilla/footer/footer.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
+import { ErrorComponent } from './plantilla/error/error.component';
+import { MisionComponent } from './plantilla/mision/mision.component';
+import { VisionComponent } from './plantilla/vision/vision.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent,
+    ErrorComponent,
+    MisionComponent,
+    VisionComponent
   ],
   imports: [
     BrowserModule,
