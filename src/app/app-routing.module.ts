9e70faf62921853577206fdc43f8270ae8e9d45c
Sebastian Gamba <sebastiangamba.music@gmail.com>
2021-11-30 16:47:28 -0500

    formulario contacto responsivo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactoComponent } from './plantilla/contacto/contacto.component';
 import { ErrorComponent } from './plantilla/error/error.component';
 import { InicioComponent } from './plantilla/inicio/inicio.component';
-import { MisionComponent } from './plantilla/mision/mision.component';
-import { VisionComponent } from './plantilla/vision/vision.component';
+
 
 const routes: Routes = [
   {
     path:"inicio",
     component: InicioComponent
-  },
-  {
-    path: "mision",
-    component: MisionComponent
-  },
-  {
-    path: "vision",
-    component: VisionComponent
   },
   {
     path: "contacto",
